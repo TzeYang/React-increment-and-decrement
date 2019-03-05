4f96048809935797d2758005abe710be713d32fa
Tze Yang Chew <tzeyangchew@MacBook-Pro.local>
2019-03-05 22:52:08 +0800

    Updates

---
+++ precache-manifest.7a62beaa18c5048ae83eef21e4cb6897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66fdd5ede66597c04c66",
+    "url": "/React-increment-and-decrement/static/js/runtime~main.66fdd5ed.js"
+  },
+  {
+    "revision": "09d31473bafe78087ab9",
+    "url": "/React-increment-and-decrement/static/js/main.09d31473.chunk.js"
+  },
+  {
+    "revision": "47108b3238107d42d02d",
+    "url": "/React-increment-and-decrement/static/js/2.47108b32.chunk.js"
+  },
+  {
+    "revision": "09d31473bafe78087ab9",
+    "url": "/React-increment-and-decrement/static/css/main.045fe62b.chunk.css"
+  },
+  {
+    "revision": "4131db514535944a8454e184b09870ca",
+    "url": "/React-increment-and-decrement/index.html"
+  }
+];
